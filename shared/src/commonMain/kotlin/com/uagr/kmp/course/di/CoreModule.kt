54a57a41e988bc63b7fa29c8ecd8b6179a2f55ab
ulises-agr <gonzalezua@hitss.com>
2026-09-06 13:22:06 -0600

    [Improvement][1.0][N/A] Comment code

--- shared/src/commonMain/kotlin/com/uagr/kmp/course/di/CoreModule.kt
+++ shared/src/commonMain/kotlin/com/uagr/kmp/course/di/CoreModule.kt
@@ -4,22 +4,13 @@
  */
 package com.uagr.kmp.course.di
 
-import com.uagr.kmp.course.data.network.client.createHttpClient
-import com.uagr.kmp.course.data.network.datasource.PackagesNetworkDataSource
-import com.uagr.kmp.course.data.network.datasource.PackagesNetworkDataSourceImpl
-import com.uagr.kmp.course.data.reposirory.PackagesRepositoryImpl
-import com.uagr.kmp.course.domain.repository.PackagesRepository
-import com.uagr.kmp.course.domain.usecase.PackagesUseCase
 import com.uagr.kmp.course.presentation.ui.packages.viewmodel.PackagesViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import org.koin.core.context.startKoin
-import org.koin.core.module.dsl.factoryOf
-import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.KoinAppDeclaration
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dispatcherModule = module {
@@ -27,19 +18,19 @@ val dispatcherModule = module {
 }
 
 val networkModule = module {
-    single { createHttpClient() }
+//    single { createHttpClient() }
 }
 
 val dataSourceRemoteModule = module {
-    singleOf(constructor = ::PackagesNetworkDataSourceImpl) bind PackagesNetworkDataSource::class
+//    singleOf(constructor = ::PackagesNetworkDataSourceImpl) bind PackagesNetworkDataSource::class
 }
 
 val repositoryModule = module {
-    singleOf(constructor = ::PackagesRepositoryImpl) bind PackagesRepository::class
+//    singleOf(constructor = ::PackagesRepositoryImpl) bind PackagesRepository::class
 }
 
 val useCaseModule = module {
-    factoryOf(constructor = ::PackagesUseCase)
+//    factoryOf(constructor = ::PackagesUseCase)
 }
 
 val viewmodelModule = module {
--- shared/src/commonMain/kotlin/com/uagr/kmp/course/utils/network/NetworkApiHelper.kt
+++ shared/src/commonMain/kotlin/com/uagr/kmp/course/utils/network/NetworkApiHelper.kt
@@ -4,63 +4,63 @@
  */
 package com.uagr.kmp.course.utils.network
 
-import com.uagr.kmp.course.data.network.model.response.base.BaseResponse
-import io.ktor.client.call.body
-import io.ktor.client.plugins.ClientRequestException
-import io.ktor.client.plugins.ServerResponseException
-import io.ktor.client.statement.HttpResponse
-import io.ktor.http.isSuccess
-import kotlinx.coroutines.TimeoutCancellationException
-import kotlinx.io.IOException
-
-suspend inline fun <reified Response : BaseResponse, Domain> safeApiCall(
-    crossinline apiCall: suspend () -> HttpResponse,
-    crossinline transform: (Response) -> Domain,
-): NetworkResult<Domain> =
-    try {
-        val response = apiCall()
-        val status = response.status.value
-        if (response.status.isSuccess()) {
-            val body = response.body<Response>()
-            if (body.success == true) {
-                NetworkResult.Success(response = transform(body))
-            } else {
-                NetworkResult.Error(
-                    message = body.message?.takeIf { message -> message.isNotBlank() } ?: "Error unknown",
-                    errorType = NetworkErrorType.UNKNOWN,
-                )
-            }
-        } else {
-            NetworkResult.Error(
-                message = "Error HTTP: $status",
-                code = status,
-                errorType = NetworkErrorType.HTTP,
-            )
-        }
-    } catch (exception: Exception) {
-        exception.printStackTrace()
-        when (exception) {
-            is TimeoutCancellationException -> NetworkResult.Error(
-                message = "Error timeout: ${exception.message}",
-                errorType = NetworkErrorType.TIMEOUT,
-            )
-            is IOException -> NetworkResult.Error(
-                message = "Error red: ${exception.message}",
-                errorType = NetworkErrorType.NETWORK,
-            )
-            is ClientRequestException -> NetworkResult.Error(
-                message = "Error client (${exception.response.status.value}): ${exception.message}",
-                code = exception.response.status.value,
-                errorType = NetworkErrorType.HTTP,
-            )
-            is ServerResponseException -> NetworkResult.Error(
-                message = "Error server (${exception.response.status.value}): ${exception.message}",
-                code = exception.response.status.value,
-                errorType = NetworkErrorType.HTTP,
-            )
-            else -> NetworkResult.Error(
-                message = "Error unknown: ${exception.message}",
-                errorType = NetworkErrorType.UNKNOWN,
-            )
-        }
-    }
+//import com.uagr.kmp.course.data.network.model.response.base.BaseResponse
+//import io.ktor.client.call.body
+//import io.ktor.client.plugins.ClientRequestException
+//import io.ktor.client.plugins.ServerResponseException
+//import io.ktor.client.statement.HttpResponse
+//import io.ktor.http.isSuccess
+//import kotlinx.coroutines.TimeoutCancellationException
+//import kotlinx.io.IOException
+//
+//suspend inline fun <reified Response : BaseResponse, Domain> safeApiCall(
+//    crossinline apiCall: suspend () -> HttpResponse,
+//    crossinline transform: (Response) -> Domain,
+//): NetworkResult<Domain> =
+//    try {
+//        val response = apiCall()
+//        val status = response.status.value
+//        if (response.status.isSuccess()) {
+//            val body = response.body<Response>()
+//            if (body.success == true) {
+//                NetworkResult.Success(response = transform(body))
+//            } else {
+//                NetworkResult.Error(
+//                    message = body.message?.takeIf { message -> message.isNotBlank() } ?: "Error unknown",
+//                    errorType = NetworkErrorType.UNKNOWN,
+//                )
+//            }
+//        } else {
+//            NetworkResult.Error(
+//                message = "Error HTTP: $status",
+//                code = status,
+//                errorType = NetworkErrorType.HTTP,
+//            )
+//        }
+//    } catch (exception: Exception) {
+//        exception.printStackTrace()
+//        when (exception) {
+//            is TimeoutCancellationException -> NetworkResult.Error(
+//                message = "Error timeout: ${exception.message}",
+//                errorType = NetworkErrorType.TIMEOUT,
+//            )
+//            is IOException -> NetworkResult.Error(
+//                message = "Error red: ${exception.message}",
+//                errorType = NetworkErrorType.NETWORK,
+//            )
+//            is ClientRequestException -> NetworkResult.Error(
+//                message = "Error client (${exception.response.status.value}): ${exception.message}",
+//                code = exception.response.status.value,
+//                errorType = NetworkErrorType.HTTP,
+//            )
+//            is ServerResponseException -> NetworkResult.Error(
+//                message = "Error server (${exception.response.status.value}): ${exception.message}",
+//                code = exception.response.status.value,
+//                errorType = NetworkErrorType.HTTP,
+//            )
+//            else -> NetworkResult.Error(
+//                message = "Error unknown: ${exception.message}",
+//                errorType = NetworkErrorType.UNKNOWN,
+//            )
+//        }
+//    }
