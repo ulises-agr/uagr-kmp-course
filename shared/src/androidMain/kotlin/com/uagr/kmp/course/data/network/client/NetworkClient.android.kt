bc5ac78681ba3de2625fea7345f082acafb273e0
ulises-agr <gonzalezua@hitss.com>
2026-09-13 15:38:52 -0600

    [Improvement][1.0][N/A] Update components

--- shared/src/androidMain/kotlin/com/uagr/kmp/course/data/network/client/NetworkClient.android.kt
+++ shared/src/androidMain/kotlin/com/uagr/kmp/course/data/network/client/NetworkClient.android.kt
@@ -4,11 +4,15 @@
  */
 package com.uagr.kmp.course.data.network.client
 
+import com.uagr.kmp.course.data.local.datastore.AppDataStore
 import com.uagr.kmp.course.utils.constant.Constants
 import com.uagr.kmp.course.utils.constant.NetworkUrl
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
@@ -16,14 +20,31 @@ import io.ktor.client.plugins.logging.Logging
 import io.ktor.http.ContentType
 import io.ktor.serialization.kotlinx.json.json
 import io.nerdythings.okhttp.profiler.OkHttpProfilerInterceptor
+import kotlinx.coroutines.flow.first
 import kotlinx.serialization.json.Json
 
-actual fun createHttpClient(): HttpClient = HttpClient(engineFactory = OkHttp) {
+actual fun createHttpClient(appDataStore: AppDataStore): HttpClient = HttpClient(engineFactory = OkHttp) {
 
     install(plugin = HttpTimeout) {
         requestTimeoutMillis = Constants.REQUEST_TIMEOUT_MILLIS
         connectTimeoutMillis = Constants.CONNECT_TIMEOUT_MILLIS
         socketTimeoutMillis = Constants.SOCKET_TIMEOUT_MILLIS
+    }
+
+    install(plugin = Auth) {
+        bearer {
+            loadTokens {
+                val token = appDataStore.userToken.first()
+                if (!token.isNullOrBlank()) {
+                    BearerTokens(
+                        accessToken = token,
+                        refreshToken = "",
+                    )
+                } else {
+                    null
+                }
+            }
+        }
     }
 
     install(plugin = Logging) {
--- shared/src/iosMain/kotlin/com/uagr/kmp/course/data/network/client/NetworkClient.ios.kt
+++ shared/src/iosMain/kotlin/com/uagr/kmp/course/data/network/client/NetworkClient.ios.kt
@@ -4,25 +4,46 @@
  */
 package com.uagr.kmp.course.data.network.client
 
+import com.uagr.kmp.course.data.local.datastore.AppDataStore
 import com.uagr.kmp.course.utils.constant.Constants
 import com.uagr.kmp.course.utils.constant.NetworkUrl
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.http.ContentType
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.flow.first
 import kotlinx.serialization.json.Json
 
-actual fun createHttpClient(): HttpClient = HttpClient(engineFactory = Darwin) {
+actual fun createHttpClient(appDataStore: AppDataStore): HttpClient = HttpClient(engineFactory = Darwin) {
 
     install(plugin = HttpTimeout) {
         requestTimeoutMillis = Constants.REQUEST_TIMEOUT_MILLIS
         connectTimeoutMillis = Constants.CONNECT_TIMEOUT_MILLIS
         socketTimeoutMillis = Constants.SOCKET_TIMEOUT_MILLIS
+    }
+
+    install(plugin = Auth) {
+        bearer {
+            loadTokens {
+                val token = appDataStore.userToken.first()
+                if (!token.isNullOrBlank()) {
+                    BearerTokens(
+                        accessToken = token,
+                        refreshToken = "",
+                    )
+                } else {
+                    null
+                }
+            }
+        }
     }
 
     install(plugin = Logging) {
