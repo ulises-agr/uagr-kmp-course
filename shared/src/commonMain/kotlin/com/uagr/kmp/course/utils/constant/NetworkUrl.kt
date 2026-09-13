04cc439b2d1c49950b2d560055a3c5382dafdee7
ulises-agr <gonzalezua@hitss.com>
2026-09-13 14:37:28 -0600

    [Improvement][1.0][N/A] Update url

--- shared/src/commonMain/kotlin/com/uagr/kmp/course/utils/constant/NetworkUrl.kt
+++ shared/src/commonMain/kotlin/com/uagr/kmp/course/utils/constant/NetworkUrl.kt
@@ -7,7 +7,7 @@ package com.uagr.kmp.course.utils.constant
 object NetworkUrl {
 
     // Base url
-    const val BASE_URL = "https://packages-course.free.beeceptor.com/"
+    const val BASE_URL = "https://server-dummy-hitss.onrender.com"
 
     // Endpoint
     const val LOGIN_ENDPOINT = "api/v1/auth/login"
